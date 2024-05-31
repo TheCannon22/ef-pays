ad3037ba3ab2b3dd6b9183dc98da3369537fbcd4
Alexander Rankov <e2259198@cmaisonneuve.qc.ca>
2024-05-31 16:10:08 -0400

    ef-AR : j'ai ajouté un peu du code pour faire apparaître les différentes catégories et les différents pays

--- pays-rest-api.php
+++ pays-rest-api.php
@@ -3,19 +3,18 @@
 Plugin Name: Pays REST API
 Description: Plugin pour générer un menu de pays et afficher les destinations via une API REST.
 Version: 1.0
-Author: Votre Nom
+Author: Alexander Rankov
 */
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+    exit;
 }
 
-// Enqueue scripts and styles
 function pays_enqueue_scripts() {
     wp_enqueue_style('pays-styles', plugin_dir_url(__FILE__) . 'css/styles.css');
     wp_enqueue_script('pays-script', plugin_dir_url(__FILE__) . 'js/pays.js', array('jquery'), null, true);
 
-    // Pass API endpoint to JavaScript
+    
     wp_localize_script('pays-script', 'paysApi', array(
         'root' => esc_url_raw(rest_url()),
         'nonce' => wp_create_nonce('wp_rest')
@@ -23,7 +22,7 @@ function pays_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'pays_enqueue_scripts');
 
-// Register REST API endpoint
+
 function enregister_pays_routes() {
     register_rest_route('pays/v1', '/destinations/', array(
         'methods' => 'GET',
@@ -62,7 +61,6 @@ function get_pays_destinations(WP_REST_Request $request) {
     return new WP_REST_Response($destinations, 200);
 }
 
-// Shortcode for country menu and destinations
 function pays_shortcode() {
     ob_start();
     ?>
